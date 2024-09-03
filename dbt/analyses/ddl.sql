e4325fed89136caa38ac1c3b8e9e5aa2091d199d
Muhammad Jarir Kanji <mjkanji@outlook.com>
2024-09-04 00:22:00 +0500

    Update DDL to:
    * drop tables first
    * create table if not exists
    * allow for duplicate couriers
    * add foreign key constraints in deliveries

--- dbt/analyses/ddl.sql
+++ dbt/analyses/ddl.sql
@@ -1,23 +1,29 @@
-CREATE TABLE raw_deliveries (
+DROP TABLE IF EXISTS raw_couriers;
+DROP TABLE IF EXISTS raw_customers;
+DROP TABLE IF EXISTS raw_deliveries;
+
+CREATE TABLE IF NOT EXISTS raw_couriers (
+    courier_id VARCHAR, -- PRIMARY KEY,
+    signup_date DATE,
+    status VARCHAR,
+    vehicle_type VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS raw_customers (
+    customer_id VARCHAR PRIMARY KEY,
+    signup_date DATE,
+    primary_payment_method VARCHAR,
+    lifetime_value DOUBLE PRECISION
+);
+
+CREATE TABLE IF NOT EXISTS raw_deliveries (
     delivery_id VARCHAR PRIMARY KEY,
     customer_id VARCHAR,
     courier_id VARCHAR,
     requested_timestamp TIMESTAMP,
     delivery_timestamp TIMESTAMP,
     delivery_fee DOUBLE PRECISION,
-    status VARCHAR
-);
-
-CREATE TABLE raw_couriers (
-    courier_id VARCHAR PRIMARY KEY,
-    signup_date DATE,
     status VARCHAR,
-    vehicle_type VARCHAR
-);
-
-CREATE TABLE raw_customers (
-    customer_id VARCHAR PRIMARY KEY,
-    signup_date DATE,
-    primary_payment_method VARCHAR,
-    lifetime_value DOUBLE PRECISION
+    FOREIGN KEY (customer_id) REFERENCES raw_customers(customer_id),
+    FOREIGN KEY (courier_id) REFERENCES raw_couriers(courier_id)
 );
